7c75e79dbedf85a0e9a971ece6ed301faed3e884
xijiao <xijiaosdq@gmail.com>
2020-03-05 22:54:51 +0900

    practise.

--- 712.minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712.minimum-ascii-delete-sum-for-two-strings.cpp
@@ -8,29 +8,20 @@
 class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
-        unordered_map<int, int> cache;
-        cache[s1.size() << 16 | s2.size()] = 0;
-        return dfs(s1, s2, cache, 0, 0);
-    }
-
-    int dfs(const string& s1, const string& s2,
-            unordered_map<int, int>& cache, int i, int j) {
-        auto it = cache.find(i << 16 | j);
-        if (it != cache.end()) return it->second;
-
-        int res = 0;
-        if (i == s1.size()) {
-            res = s2[j] + dfs(s1, s2, cache, i, j + 1);
-        } else if (j == s2.size()) {
-            res = s1[i] + dfs(s1, s2, cache, i + 1, j);
-        } else if (s1[i] == s2[j]) {
-            res = dfs(s1, s2, cache, i + 1, j + 1);
-        } else {
-            res = min(s1[i] + dfs(s1, s2, cache, i + 1, j),
-                    s2[j] + dfs(s1, s2, cache, i, j + 1));
+        int dp[s1.size() + 1];
+        dp[0] = 0;
+        for (int i = 0; i < s1.size(); i++) dp[i + 1] = dp[i] + s1[i];
+        for (int i = 1; i < s2.size() + 1; i++) {
+            int t = dp[0];
+            dp[0] += s2[i - 1];
+            for (int j = 1; j < s1.size() + 1; j++) {
+                int p = dp[j];
+                if (s1[j - 1] == s2[i - 1]) dp[j] = t;
+                else dp[j] = min(dp[j - 1] + s1[j - 1], dp[j] + s2[i - 1]);
+                t = p;
+            }
         }
-        cache[i << 16 | j] = res;
-        return res;
+        return dp[s1.size()];
     }
 };
 // @lc code=end
