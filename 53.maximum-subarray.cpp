8fceb1dc0ebe9737837797cabe4feaa6ae80f30e
xijiao <xijiaosdq@gmail.com>
2021-02-11 11:39:14 +0900

    Refinished 53.

--- 53.maximum-subarray.cpp
+++ 53.maximum-subarray.cpp
@@ -5,25 +5,52 @@
  */
 
 // @lc code=start
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if (nums.empty()) return 0;
-        int max_sa = nums[0];
-        int former_min = min(0, nums[0]);
-        int cur = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            cur += nums[i];
-            if (cur - former_min > max_sa) {
-                max_sa = cur - former_min;
-            }
-            if (cur < former_min) {
-                former_min = cur;
-            }
+        int max_sum = nums[0];
+        int cur_sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            cur_sum += nums[i];
+            max_sum = max(max_sum, cur_sum);
+            if (cur_sum < 0) cur_sum = 0;
         }
 
-        return max_sa;
+        return max_sum;
     }
 };
+
+/*
+// Divide and Conquer
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArray(nums, 0, nums.size() - 1);
+    }
+public:
+    int maxSubArray(vector<int>& nums, int l, int r) {
+        if (l > r) return numeric_limits<int>::min();
+        if (l == r) return nums[l];
+
+        int m = (l + r) / 2;
+        int maxl = maxSubArray(nums, l, m - 1);
+        int maxr = maxSubArray(nums, m + 1, r);
+
+        int maxsubl = 0, maxsubr = 0;
+        for (int i = m - 1, sum = 0; i >= 0; i--) {
+            sum += nums[i];
+            maxsubl = max(maxsubl, sum);
+        }
+        for (int i = m + 1, sum = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            maxsubr = max(maxsubr, sum);
+        }
+        
+        return max(max(maxl, maxr), nums[m] + maxsubl + maxsubr);
+    }
+};
+*/
+
 // @lc code=end
 
